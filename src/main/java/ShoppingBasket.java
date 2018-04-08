b9a466a170566dda79ed292bfce0656a2d644515
Arthur Andreev <arthur.mailbox@yandex.com>
2018-04-08 20:35:39 +0100

    refactored calculate total method, all tests are passing

--- src/main/java/ShoppingBasket.java
+++ src/main/java/ShoppingBasket.java
@@ -28,7 +28,7 @@ public class ShoppingBasket {
         this.products.clear();
     }
 
-    //subtotal method calculates total value of basket excluding any discounts
+    // calculates total value of basket excluding any discounts due
     public double subtotal() {
         ArrayList<Product> shoppingCart = new ArrayList<>();
         double total = 0;
@@ -39,7 +39,32 @@ public class ShoppingBasket {
 
         return total;
     }
-//highspender discount method
+
+    //calculates total price with all relevant discounts due
+    public double calculateTotal(Customer customer, Product item) {
+        double total = subtotal();
+
+        switch(discountType) {
+
+            case HIGHSPENDER:
+                //high spenders get a discount of 10%
+                highSpenderDiscount();
+                break;
+
+            case LOYALTYCARD:
+                //loyalty card holders get a discount of 2%
+                loyaltyCardHolderDiscount(customer);
+                break;
+        }
+        return total;
+
+////        buy one get one free method discount does not work
+//        if (item.getBuyOneGetOneFreeItem() == true){
+//            products.add(item);
+//            total -= (total - item.getPrice());
+        }
+
+    //highspender discount method
     public double highSpenderDiscount() {
         double total = subtotal();
         if (total > 20.00) {
@@ -47,32 +72,13 @@ public class ShoppingBasket {
         }
         return total;
     }
-//loyalty cardholder discount method
+
+    //loyalty cardholder discount method
     public double loyaltyCardHolderDiscount(Customer customer) {
         double total = subtotal();
         if (customer.getLoyaltyCardHolderStatus() == true) {
             total -= (total * discountType.LOYALTYCARD.getValue());
         }
-        return total;
-    }
-        //calculates total price of basket including all the relevant discounts
-    public double calculateTotal(Customer customer, Product item) {
-        double total = subtotal();
-
-        //high spenders who spend over 20 pounds get a discount of 10%
-        if (total > 20.00) {
-            total -= (total * discountType.HIGHSPENDER.getValue());
-        }
-
-        //loyalty card holders get a discount of 2%
-        if (customer.getLoyaltyCardHolderStatus() == true) {
-            total -= (total * discountType.LOYALTYCARD.getValue());
-        }
-        //buy one get one free method discount
-//        if (item.getBuyOneGetOneFreeItem() == true){
-//            products.add(item);
-//            total -= (total - item.getPrice());
-//        }
         return total;
     }
 
